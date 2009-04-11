9ca2603890d0fc76f31f9a18372b98f1db4e276f
Marano <thiagomarano@gmail.com>
2009-04-11 13:54:39 -0300

    blank redirect page

--- vendor/plugins/redirect_fu/lib/redirect_fu.rb
+++ vendor/plugins/redirect_fu/lib/redirect_fu.rb
@@ -3,7 +3,7 @@ module RedirectFu
   def redirect_from_post(to = '', params = {})
     @parameters = params
     @parameters[:to] = to
-    render 'redirect/to'
+    render 'redirect/to', :layout => false
   end
   
 end
