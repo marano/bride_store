d063dee136314a7b4cd421badbec889f4a839c91
Marano <thiagomarano@gmail.com>
2009-04-17 00:35:43 -0300

    added newsletter field

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090416023547) do
+ActiveRecord::Schema.define(:version => 20090417033439) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -219,7 +219,7 @@ ActiveRecord::Schema.define(:version => 20090416023547) do
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
-    t.string   "name",                      :limit => 100, :default => ""
+    t.string   "name",                      :limit => 100
     t.string   "email",                     :limit => 100
     t.string   "crypted_password",          :limit => 40
     t.string   "salt",                      :limit => 40
@@ -229,13 +229,11 @@ ActiveRecord::Schema.define(:version => 20090416023547) do
     t.datetime "remember_token_expires_at"
     t.string   "activation_code",           :limit => 40
     t.datetime "activated_at"
-    t.string   "state",                                    :default => "passive"
+    t.string   "state"
     t.datetime "deleted_at"
     t.boolean  "admin"
-    t.boolean  "newsletter"
     t.string   "phone"
+    t.boolean  "newsletter"
   end
-
-  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
 
 end
